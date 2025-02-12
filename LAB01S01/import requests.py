6d16bfba4af167d8fef39befcfde7fd42ebb48f2
Gabriel Vitor <gabrielvitor0309@gmail.com>
2025-02-12 11:33:35 -0300

    feat: Update query to search for repositories with more than 100,000 stars

--- LAB01S01/import requests.py
+++ LAB01S01/import requests.py
@@ -10,7 +10,7 @@ def get_query(after_cursor=None):
     after = f', after: "{after_cursor}"' if after_cursor else ''
     return f"""
     {{
-      search(query: "stars:>10000", type: REPOSITORY, first: 10{after}) {{
+      search(query: "stars:>100000", type: REPOSITORY, first: 10{after}) {{
         edges {{
           cursor
           node {{
